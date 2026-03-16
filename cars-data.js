48e7c0b68f5673758b291ca8c3e4386f85f4e6b8
avtobazapro <nikolay_nee@mail.ru>
2026-03-16 16:39:55 +0300

    Create cars-data.js

---
+++ cars-data.js
@@ -0,0 +1,700 @@
+window.CAR_DATA = {
+  "hyundai": {
+    label: "Hyundai",
+    models: {
+      "Accent": ["1.4 Gasoline", "1.6 Gasoline"],
+      "Avante": ["1.6 Gasoline", "1.6 Hybrid", "2.0 Gasoline", "N Line"],
+      "Azera": ["2.5 Gasoline", "3.0 LPG"],
+      "Grandeur": ["2.5 Gasoline", "3.5 Gasoline", "1.6 Hybrid", "3.5 LPG"],
+      "Ioniq 5": ["Standard Range", "Long Range", "Long Range AWD"],
+      "Ioniq 6": ["Standard Range", "Long Range", "Long Range AWD"],
+      "Kona": ["1.6 Turbo", "2.0 Gasoline", "Electric"],
+      "Palisade": ["2.2 Diesel", "3.8 Gasoline"],
+      "Santa Fe": ["2.2 Diesel", "2.5 Turbo", "1.6 Hybrid"],
+      "Sonata": ["1.6 Turbo", "2.0 Gasoline", "2.0 LPG", "2.5 Gasoline"],
+      "Staria": ["2.2 Diesel", "3.5 LPG", "3.5 Gasoline"],
+      "Tucson": ["1.6 Turbo", "2.0 Diesel", "1.6 Hybrid", "2.0 Gasoline"]
+    }
+  },
+
+  "genesis": {
+    label: "Genesis",
+    models: {
+      "G70": ["2.0 Gasoline", "2.2 Diesel", "3.3 Turbo"],
+      "G80": ["2.5 Turbo", "3.5 Turbo", "2.2 Diesel"],
+      "G90": ["3.5 Turbo", "3.5 Turbo AWD", "5.0 AWD"],
+      "GV60": ["Standard", "Performance AWD"],
+      "GV70": ["2.5 Turbo", "3.5 Turbo", "2.2 Diesel", "Electric"],
+      "GV80": ["2.5 Turbo", "3.5 Turbo", "3.0 Diesel"]
+    }
+  },
+
+  "kia": {
+    label: "Kia",
+    models: {
+      "Carnival": ["2.2 Diesel Prestige", "2.2 Diesel Signature", "3.5 Gasoline Signature"],
+      "EV6": ["Standard Range", "Long Range", "Long Range AWD", "GT-Line", "GT"],
+      "K5": ["1.6 Turbo", "2.0 Gasoline", "2.0 LPG", "2.0 Hybrid", "2.5 GT"],
+      "K7": ["2.5 Gasoline", "3.0 LPG", "3.3 Gasoline"],
+      "K8": ["2.5 Gasoline", "3.5 Gasoline", "1.6 Hybrid", "3.5 LPG"],
+      "K9": ["3.3 Turbo", "3.8 Gasoline", "5.0 AWD"],
+      "Mohave": ["3.0 Diesel Master", "3.0 Diesel Gravity"],
+      "Morning": ["1.0 Gasoline", "1.0 Turbo"],
+      "Niro": ["1.6 Hybrid", "1.6 Plug-in Hybrid", "EV"],
+      "Ray": ["1.0 Gasoline", "EV"],
+      "Seltos": ["1.6 Turbo", "2.0 Gasoline", "1.6 Diesel"],
+      "Sorento": ["2.2 Diesel", "2.5 Turbo", "1.6 Hybrid", "3.5 Gasoline"],
+      "Soul": ["1.6 Gasoline", "2.0 Gasoline", "EV"],
+      "Sportage": ["1.6 Turbo", "2.0 Diesel", "1.6 Hybrid", "2.0 Gasoline", "2.5 Gasoline"],
+      "Stinger": ["2.0 Turbo", "2.2 Diesel", "3.3 Turbo GT"]
+    }
+  },
+
+  "chevrolet_gm_daewoo": {
+    label: "Chevrolet (GM Daewoo)",
+    models: {
+      "Spark": ["1.0 Gasoline"],
+      "Malibu": ["1.35 Turbo", "1.5 Turbo", "2.0 Turbo"],
+      "Trax": ["1.2 Turbo", "1.4 Turbo"],
+      "Traverse": ["3.6 Gasoline"],
+      "Tahoe": ["5.3 Gasoline", "6.2 Gasoline"],
+      "Camaro": ["2.0 Turbo", "3.6 Gasoline", "6.2 V8"]
+    }
+  },
+
+  "renault_korea": {
+    label: "Renault Korea",
+    models: {
+      "Arkana": ["1.3 Turbo", "1.6 Hybrid"],
+      "QM6": ["2.0 Gasoline", "2.0 LPG"],
+      "SM6": ["1.3 Turbo", "2.0 Gasoline"],
+      "XM3": ["1.3 Turbo", "1.6 Hybrid"]
+    }
+  },
+
+  "kg_mobility_ssangyong": {
+    label: "KG Mobility (SsangYong)",
+    models: {
+      "Korando": ["1.5 Turbo", "1.6 Diesel"],
+      "Rexton": ["2.2 Diesel"],
+      "Torres": ["1.5 Turbo"],
+      "Tivoli": ["1.5 Gasoline"]
+    }
+  },
+
+  "bmw": {
+    label: "BMW",
+    models: {
+      "1 Series": ["118i", "120i", "120d"],
+      "2 Series": ["218i", "220i", "220d", "M240i"],
+      "3 Series": ["320i", "320d", "330i", "330e", "M340i"],
+      "4 Series": ["420i", "420d", "430i", "M440i"],
+      "5 Series": ["520i", "520d", "530i", "530e", "540i"],
+      "7 Series": ["730d", "740i", "750e", "760i"],
+      "X1": ["18i", "20i", "18d", "20d"],
+      "X3": ["20i", "20d", "30e", "M40i"],
+      "X4": ["20i", "20d", "M40i"],
+      "X5": ["30d xDrive", "40i xDrive", "45e xDrive", "50e xDrive", "M50i"],
+      "X6": ["30d xDrive", "40i xDrive", "M50i"],
+      "X7": ["30d xDrive", "40i xDrive", "M60i"],
+      "i4": ["eDrive35", "eDrive40", "M50"],
+      "i5": ["eDrive40", "M60"],
+      "iX": ["xDrive40", "xDrive50", "M60"]
+    }
+  },
+
+  "byd": {
+    label: "BYD",
+    models: {
+      "Atto 3": ["EV"],
+      "Han": ["EV"],
+      "Tang": ["EV"]
+    }
+  },
+
+  "gmc": {
+    label: "GMC",
+    models: {
+      "Acadia": ["2.0 Gasoline", "2.5 Gasoline", "3.6 Gasoline"],
+      "Sierra": ["2.7 Turbo", "3.0 Diesel", "5.3 Gasoline", "6.2 Gasoline"],
+      "Terrain": ["1.5 Turbo", "2.0 Turbo"],
+      "Yukon": ["5.3 Gasoline", "6.2 Gasoline"]
+    }
+  },
+
+  "nissan": {
+    label: "Nissan",
+    models: {
+      "Altima": ["2.0 Turbo", "2.5 Gasoline"],
+      "Kicks": ["1.6 Gasoline"],
+      "Murano": ["2.5 Gasoline", "3.5 Gasoline"],
+      "Pathfinder": ["3.5 Gasoline"],
+      "Rogue": ["1.5 Turbo", "2.0 Gasoline", "2.5 Gasoline"],
+      "Sentra": ["1.6 Gasoline", "2.0 Gasoline"]
+    }
+  },
+
+  "daihatsu": {
+    label: "Daihatsu",
+    models: {
+      "Mira": ["0.7 Gasoline"],
+      "Move": ["0.7 Gasoline"],
+      "Terios": ["1.5 Gasoline"]
+    }
+  },
+
+  "dodge": {
+    label: "Dodge",
+    models: {
+      "Challenger": ["3.6 Gasoline", "5.7 Gasoline", "6.4 Gasoline"],
+      "Charger": ["3.6 Gasoline", "5.7 Gasoline", "6.4 Gasoline"],
+      "Durango": ["3.6 Gasoline", "5.7 Gasoline"]
+    }
+  },
+
+  "toyota": {
+    label: "Toyota",
+    models: {
+      "Alphard": ["2.5 Hybrid", "3.5 Gasoline"],
+      "Camry": ["2.0 Gasoline", "2.5 Gasoline", "2.5 Hybrid", "3.5 Gasoline"],
+      "Corolla": ["1.6 Gasoline", "1.8 Hybrid", "2.0 Gasoline"],
+      "Highlander": ["2.4 Turbo", "2.5 Hybrid", "3.5 Gasoline"],
+      "Land Cruiser": ["3.3 Diesel", "3.5 Gasoline", "4.0 Gasoline"],
+      "RAV4": ["2.0 Gasoline", "2.5 Gasoline", "2.5 Hybrid"],
+      "Sienna": ["2.5 Hybrid", "3.5 Gasoline"]
+    }
+  },
+
+  "dongfeng_sokon": {
+    label: "Dongfeng Sokon",
+    models: {
+      "Glory 580": ["1.5 Turbo"],
+      "Glory ix5": ["1.5 Turbo"]
+    }
+  },
+
+  "lamborghini": {
+    label: "Lamborghini",
+    models: {
+      "Huracan": ["5.2 V10"],
+      "Urus": ["4.0 Turbo"],
+      "Aventador": ["6.5 V12"]
+    }
+  },
+
+  "land_rover": {
+    label: "Land Rover",
+    models: {
+      "Defender": ["2.0 Gasoline", "3.0 Gasoline", "5.0 Gasoline"],
+      "Discovery": ["2.0 Gasoline", "3.0 Gasoline"],
+      "Discovery Sport": ["2.0 Gasoline"],
+      "Range Rover": ["3.0 Gasoline", "4.4 Gasoline", "5.0 Gasoline"],
+      "Range Rover Sport": ["3.0 Gasoline", "4.4 Gasoline", "5.0 Gasoline"],
+      "Range Rover Velar": ["2.0 Gasoline", "3.0 Gasoline"]
+    }
+  },
+
+  "lexus": {
+    label: "Lexus",
+    models: {
+      "ES": ["250", "300h", "350"],
+      "GX": ["460"],
+      "IS": ["300", "350"],
+      "LS": ["500", "500h"],
+      "LX": ["500d", "600"],
+      "NX": ["250", "350", "350h", "450h+"],
+      "RX": ["350", "350h", "500h"],
+      "UX": ["200", "250h"]
+    }
+  },
+
+  "mg_rover": {
+    label: "MG Rover",
+    models: {
+      "MG5": ["1.5 Gasoline"],
+      "MG HS": ["1.5 Turbo"]
+    }
+  },
+
+  "lotus": {
+    label: "Lotus",
+    models: {
+      "Emira": ["2.0 Turbo", "3.5 V6"],
+      "Eletre": ["EV"]
+    }
+  },
+
+  "rolls_royce": {
+    label: "Rolls-Royce",
+    models: {
+      "Cullinan": ["6.75 V12"],
+      "Ghost": ["6.75 V12"],
+      "Phantom": ["6.75 V12"]
+    }
+  },
+
+  "renault": {
+    label: "Renault",
+    models: {
+      "Arkana": ["1.3 Turbo", "1.6 Hybrid"],
+      "Koleos": ["2.0 Gasoline", "2.5 Gasoline"]
+    }
+  },
+
+  "lincoln": {
+    label: "Lincoln",
+    models: {
+      "Aviator": ["3.0 Turbo"],
+      "Corsair": ["2.0 Turbo", "2.3 Turbo"],
+      "Navigator": ["3.5 Turbo"]
+    }
+  },
+
+  "maserati": {
+    label: "Maserati",
+    models: {
+      "Ghibli": ["2.0 Hybrid", "3.0 V6"],
+      "Levante": ["2.0 Hybrid", "3.0 V6", "3.8 V8"],
+      "Quattroporte": ["3.0 V6", "3.8 V8"]
+    }
+  },
+
+  "maybach": {
+    label: "Maybach",
+    models: {
+      "S-Class": ["S 580", "S 680"]
+    }
+  },
+
+  "mazda": {
+    label: "Mazda",
+    models: {
+      "CX-30": ["2.0 Gasoline", "2.5 Gasoline"],
+      "CX-5": ["2.0 Gasoline", "2.5 Gasoline"],
+      "CX-9": ["2.5 Turbo"],
+      "Mazda3": ["1.5 Gasoline", "2.0 Gasoline"],
+      "Mazda6": ["2.0 Gasoline", "2.5 Gasoline"]
+    }
+  },
+
+  "mclaren": {
+    label: "McLaren",
+    models: {
+      "570S": ["3.8 Turbo"],
+      "720S": ["4.0 Turbo"],
+      "GT": ["4.0 Turbo"]
+    }
+  },
+
+  "mercury": {
+    label: "Mercury",
+    models: {}
+  },
+
+  "mini": {
+    label: "Mini",
+    models: {
+      "Clubman": ["Cooper", "Cooper S", "John Cooper Works"],
+      "Convertible": ["Cooper", "Cooper S"],
+      "Cooper": ["1.5 Turbo", "Cooper S 2.0", "John Cooper Works"],
+      "Countryman": ["Cooper", "Cooper S", "John Cooper Works"]
+    }
+  },
+
+  "mitsubishi": {
+    label: "Mitsubishi",
+    models: {
+      "Outlander": ["2.0 Gasoline", "2.4 Gasoline", "2.5 Gasoline"],
+      "Pajero Sport": ["2.4 Diesel", "3.0 Gasoline"]
+    }
+  },
+
+  "mitsuoka": {
+    label: "Mitsuoka",
+    models: {
+      "Buddy": ["2.0 Gasoline"]
+    }
+  },
+
+  "mercedes_benz": {
+    label: "Mercedes-Benz",
+    models: {
+      "A-Class": ["A 200", "A 220", "A 250 4MATIC"],
+      "C-Class": ["C 200", "C 220d", "C 300", "C 43 AMG"],
+      "CLA": ["CLA 200", "CLA 250 4MATIC", "CLA 45 AMG"],
+      "CLS": ["CLS 300d", "CLS 450 4MATIC", "CLS 53 AMG"],
+      "E-Class": ["E 220d", "E 300", "E 450 4MATIC", "E 53 AMG"],
+      "EQE": ["EQE 300", "EQE 350+", "EQE 500 4MATIC"],
+      "EQS": ["EQS 450+", "EQS 580 4MATIC"],
+      "G-Class": ["G 350d", "G 400d", "G 500", "G 63 AMG"],
+      "GLA": ["GLA 200", "GLA 220", "GLA 250 4MATIC", "GLA 35 AMG"],
+      "GLB": ["GLB 200", "GLB 220d", "GLB 250 4MATIC", "GLB 35 AMG"],
+      "GLC": ["GLC 220d", "GLC 300", "GLC 300e", "GLC 43 AMG"],
+      "GLE": ["GLE 300d", "GLE 400d", "GLE 450 4MATIC", "GLE 450 AMG Line", "GLE 53 AMG", "GLE 63 AMG"],
+      "GLS": ["GLS 350d", "GLS 400d", "GLS 450", "GLS 580", "GLS 63 AMG"],
+      "S-Class": ["S 350d", "S 400d", "S 450 4MATIC", "S 580 4MATIC", "S 63 AMG"]
+    }
+  },
+
+  "bentley": {
+    label: "Bentley",
+    models: {
+      "Bentayga": ["3.0 Hybrid", "4.0 V8", "6.0 W12"],
+      "Continental GT": ["4.0 V8", "6.0 W12"],
+      "Flying Spur": ["2.9 Hybrid", "4.0 V8", "6.0 W12"]
+    }
+  },
+
+  "volvo": {
+    label: "Volvo",
+    models: {
+      "S60": ["B4", "B5", "T8 Recharge"],
+      "S90": ["B5", "T8 Recharge"],
+      "XC40": ["B3", "B4", "Recharge"],
+      "XC60": ["B5", "B6", "T8 Recharge"],
+      "XC90": ["B5", "B6", "T8 Recharge"]
+    }
+  },
+
+  "bugatti": {
+    label: "Bugatti",
+    models: {
+      "Chiron": ["8.0 W16"]
+    }
+  },
+
+  "buick": {
+    label: "Buick",
+    models: {
+      "Encore": ["1.3 Turbo"],
+      "Enclave": ["3.6 Gasoline"]
+    }
+  },
+
+  "saab": {
+    label: "Saab",
+    models: {}
+  },
+
+  "science": {
+    label: "Scienne",
+    models: {}
+  },
+
+  "xin_yuan": {
+    label: "Xin Yuan",
+    models: {}
+  },
+
+  "saturn": {
+    label: "Saturn",
+    models: {}
+  },
+
+  "chevrolet": {
+    label: "Chevrolet",
+    models: {
+      "Malibu": ["1.35 Turbo", "1.5 Turbo", "2.0 Turbo"],
+      "Tahoe": ["5.3 Gasoline", "6.2 Gasoline"],
+      "Traverse": ["3.6 Gasoline"],
+      "Camaro": ["2.0 Turbo", "3.6 Gasoline", "6.2 V8"]
+    }
+  },
+
+  "smart": {
+    label: "Smart",
+    models: {
+      "Fortwo": ["1.0 Gasoline", "EV"]
+    }
+  },
+
+  "subaru": {
+    label: "Subaru",
+    models: {
+      "Forester": ["2.0 Gasoline", "2.5 Gasoline"],
+      "Outback": ["2.5 Gasoline"],
+      "XV": ["2.0 Gasoline"]
+    }
+  },
+
+  "suzuki": {
+    label: "Suzuki",
+    models: {
+      "Jimny": ["1.5 Gasoline"],
+      "Swift": ["1.2 Gasoline"]
+    }
+  },
+
+  "citroen_ds": {
+    label: "Citroën/DS",
+    models: {
+      "C5 Aircross": ["1.6 Turbo"],
+      "DS7": ["1.6 Turbo"]
+    }
+  },
+
+  "audi": {
+    label: "Audi",
+    models: {
+      "A3": ["A3 35 TFSI", "A3 40 TFSI", "A3 Sportback 35 TFSI"],
+      "A4": ["A4 35 TFSI", "A4 40 TFSI", "A4 45 TFSI quattro"],
+      "A5": ["A5 40 TFSI", "A5 45 TFSI quattro"],
+      "A6": ["A6 40 TDI", "A6 45 TFSI", "A6 50 TDI quattro"],
+      "A7": ["A7 45 TFSI", "A7 50 TDI quattro"],
+      "A8": ["A8 50 TDI", "A8 55 TFSI", "A8 L 60 TFSI"],
+      "Q3": ["Q3 35 TFSI", "Q3 40 TFSI quattro"],
+      "Q5": ["Q5 40 TDI", "Q5 45 TFSI quattro"],
+      "Q7": ["Q7 45 TDI", "Q7 50 TDI", "Q7 55 TFSI"],
+      "Q8": ["Q8 50 TDI", "Q8 55 TFSI"],
+      "e-tron": ["e-tron 50 quattro", "e-tron 55 quattro"]
+    }
+  },
+
+  "alfa_romeo": {
+    label: "Alfa Romeo",
+    models: {
+      "Giulia": ["2.0 Turbo", "2.9 QV"],
+      "Stelvio": ["2.0 Turbo", "2.9 QV"]
+    }
+  },
+
+  "aston_martin": {
+    label: "Aston Martin",
+    models: {
+      "DB11": ["4.0 V8", "5.2 V12"],
+      "DBX": ["4.0 V8"],
+      "Vantage": ["4.0 V8"]
+    }
+  },
+
+  "acura": {
+    label: "Acura",
+    models: {
+      "MDX": ["3.5 Gasoline"],
+      "RDX": ["2.0 Turbo"],
+      "TLX": ["2.0 Turbo"]
+    }
+  },
+
+  "opel": {
+    label: "Opel",
+    models: {
+      "Insignia": ["2.0 Gasoline"],
+      "Mokka": ["1.2 Turbo"]
+    }
+  },
+
+  "oldsmobile": {
+    label: "Oldsmobile",
+    models: {}
+  },
+
+  "ineos": {
+    label: "Ineos",
+    models: {
+      "Grenadier": ["3.0 Gasoline", "3.0 Diesel"]
+    }
+  },
+
+  "isuzu": {
+    label: "Isuzu",
+    models: {
+      "D-Max": ["3.0 Diesel"]
+    }
+  },
+
+  "infiniti": {
+    label: "Infiniti",
+    models: {
+      "Q50": ["2.0 Turbo", "3.0 Turbo"],
+      "Q60": ["2.0 Turbo", "3.0 Turbo"],
+      "QX50": ["2.0 Turbo"],
+      "QX60": ["3.5 Gasoline"],
+      "QX80": ["5.6 Gasoline"]
+    }
+  },
+
+  "jaguar": {
+    label: "Jaguar",
+    models: {
+      "E-Pace": ["2.0 Gasoline"],
+      "F-Pace": ["2.0 Gasoline", "3.0 Gasoline", "5.0 Gasoline"],
+      "XF": ["2.0 Gasoline", "3.0 Gasoline"]
+    }
+  },
+
+  "geography": {
+    label: "Geely",
+    models: {
+      "Coolray": ["1.5 Turbo"],
+      "Monjaro": ["2.0 Turbo"]
+    }
+  },
+
+  "jeep": {
+    label: "Jeep",
+    models: {
+      "Cherokee": ["2.0 Turbo", "2.4 Gasoline", "3.2 Gasoline"],
+      "Compass": ["1.3 Turbo", "2.0 Gasoline", "2.4 Gasoline"],
+      "Grand Cherokee": ["2.0 Turbo", "3.6 Gasoline", "5.7 Gasoline", "6.4 Gasoline"],
+      "Wrangler": ["2.0 Turbo", "3.6 Gasoline"]
+    }
+  },
+
+  "cadillac": {
+    label: "Cadillac",
+    models: {
+      "CT4": ["2.0 Turbo", "2.7 Turbo", "3.6 Gasoline"],
+      "CT5": ["2.0 Turbo", "3.0 Turbo", "6.2 V8"],
+      "Escalade": ["6.2 Gasoline"],
+      "XT4": ["2.0 Turbo"],
+      "XT5": ["2.0 Turbo", "3.6 Gasoline"],
+      "XT6": ["2.0 Turbo", "3.6 Gasoline"]
+    }
+  },
+
+  "koenigsegg": {
+    label: "Koenigsegg",
+    models: {
+      "Jesko": ["5.0 V8"],
+      "Regera": ["5.0 V8 Hybrid"]
+    }
+  },
+
+  "chrysler": {
+    label: "Chrysler",
+    models: {
+      "300": ["3.6 Gasoline", "5.7 Gasoline", "6.4 Gasoline"],
+      "Pacifica": ["3.6 Gasoline"]
+    }
+  },
+
+  "tesla": {
+    label: "Tesla",
+    models: {
+      "Model 3": ["RWD", "Long Range AWD", "Performance"],
+      "Model S": ["Long Range", "Plaid"],
+      "Model X": ["Long Range", "Plaid"],
+      "Model Y": ["RWD", "Long Range AWD", "Performance"]
+    }
+  },
+
+  "pagani": {
+    label: "Pagani",
+    models: {
+      "Huayra": ["6.0 V12"],
+      "Utopia": ["6.0 V12"]
+    }
+  },
+
+  "ferrari": {
+    label: "Ferrari",
+    models: {
+      "296 GTB": ["3.0 Hybrid"],
+      "Roma": ["3.9 Turbo"],
+      "SF90": ["4.0 Hybrid"],
+      "Portofino": ["3.9 Turbo"]
+    }
+  },
+
+  "ford": {
+    label: "Ford",
+    models: {
+      "Escape": ["1.5 Turbo", "2.0 Turbo", "2.5 Hybrid"],
+      "Explorer": ["2.3 Turbo", "3.0 Turbo"],
+      "Expedition": ["3.5 Turbo"],
+      "F-150": ["2.7 Turbo", "3.5 Turbo", "5.0 V8"],
+      "Mustang": ["2.3 Turbo", "5.0 V8"]
+    }
+  },
+
+  "porsche": {
+    label: "Porsche",
+    models: {
+      "718 Boxster": ["2.0", "2.5 S", "4.0 GTS"],
+      "718 Cayman": ["2.0", "2.5 S", "4.0 GTS", "GT4"],
+      "Cayenne": ["3.0", "E-Hybrid", "S", "Turbo GT"],
+      "Macan": ["2.0", "S", "GTS"],
+      "Panamera": ["2.9 4", "4S", "Turbo S", "E-Hybrid"],
+      "Taycan": ["RWD", "4S", "Turbo", "Turbo S"]
+    }
+  },
+
+  "photon": {
+    label: "Foton",
+    models: {
+      "Tunland": ["2.0 Diesel"]
+    }
+  },
+
+  "volkswagen": {
+    label: "Volkswagen",
+    models: {
+      "Arteon": ["2.0 TSI"],
+      "Golf": ["1.4 TSI", "2.0 TDI", "GTI", "R"],
+      "Jetta": ["1.4 TSI", "1.5 TSI", "2.0 TSI"],
+      "Passat": ["1.4 TSI", "2.0 TDI", "2.0 TSI"],
+      "Tiguan": ["1.4 TSI", "2.0 TDI", "2.0 TSI"],
+      "Touareg": ["2.0 TSI", "3.0 TDI", "3.0 TSI"]
+    }
+  },
+
+  "pontiac": {
+    label: "Pontiac",
+    models: {}
+  },
+
+  "polestar": {
+    label: "Polestar",
+    models: {
+      "Polestar 2": ["EV"],
+      "Polestar 3": ["EV"]
+    }
+  },
+
+  "peugeot": {
+    label: "Peugeot",
+    models: {
+      "3008": ["1.6 Turbo"],
+      "5008": ["1.6 Turbo"],
+      "508": ["1.6 Turbo"]
+    }
+  },
+
+  "fiat": {
+    label: "Fiat",
+    models: {
+      "500": ["1.2 Gasoline", "EV"],
+      "500X": ["1.3 Turbo"]
+    }
+  },
+
+  "hummer": {
+    label: "Hummer",
+    models: {
+      "H2": ["6.0 V8", "6.2 V8"],
+      "EV Pickup": ["EV"]
+    }
+  },
+
+  "honda": {
+    label: "Honda",
+    models: {
+      "Accord": ["1.5 Turbo", "2.0 Turbo"],
+      "CR-V": ["1.5 Turbo", "2.0 Gasoline", "2.4 Gasoline"],
+      "Civic": ["1.5 Turbo", "2.0 Gasoline"],
+      "Odyssey": ["3.5 Gasoline"],
+      "Pilot": ["3.5 Gasoline"]
+    }
+  },
+
+  "other_manufacturers": {
+    label: "Other manufacturers",
+    models: {}
+  },
+
+  "other_imported_cars": {
+    label: "Other imported cars",
+    models: {}
+  }
+};
